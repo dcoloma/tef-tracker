21926b20a019614f969a75fc3658540fc996dedf
dcoloma <dcoloma@dcolomas-MacBook-Pro.local>
2013-11-28 10:42:19 +0100

    removing unused files

--- dashboard/main.js
+++
@@ -1,9 +0,0 @@
-chrome.app.runtime.onLaunched.addListener(function() {
-  chrome.app.window.create('home.html', {
-          id: "FirefoxOS",
-    bounds: {
-      width: 500,
-      height: 320
-    }
-  });
-});
