e798989b4fecd02e5d5d7bf1b5a7dd3187f00027
dcoloma <dcoloma@dcolomas-MacBook-Pro.local>
2013-11-30 10:08:14 +0100

    manifest and appcache issues

--- dashboard/js/add2home.js
+++ dashboard/js/add2home.js
@@ -193,7 +193,7 @@ var addToHome = (function (w) {
             startY = w.innerHeight + w.scrollY;
 
 				startX = Math.round((w.innerWidth - balloon.offsetWidth) / 2) + w.scrollX;
-				balloon.style.left = startX+80 + 'px';
+				balloon.style.left = startX+60 + 'px';
 				balloon.style.top = startY - balloon.offsetHeight - options.bottomOffset + 'px';
 
 			switch ( options.animationIn ) {
